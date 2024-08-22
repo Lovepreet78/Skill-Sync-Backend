393e9b01421f090b643a555e3e7368ca7c2a085e
Lovepreet Singh <lovepreetsandhu78885@gmail.com>
2024-08-22 10:59:34 +0530

    first commit

---
+++ src/main/java/com/lovepreet/skill_sync/SkillSyncApplication.java
@@ -0,0 +1,13 @@
+package com.lovepreet.skill_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSyncApplication.class, args);
+	}
+
+}
